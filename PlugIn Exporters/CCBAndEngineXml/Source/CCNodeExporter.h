23a0364352856d643a5e7a2bb93453d25282d958
Nicolas Gramlich <ngramlich@zynga.com>
2012-04-30 12:31:20 -0700

    Added basic structure for exporting the actual scenegraph.

---
+++ PlugIn Exporters/CCBAndEngineXml/Source/CCNodeExporter.h
@@ -0,0 +1,13 @@
+//
+//  CCNodeExporter.h
+//  CCBAndEngineXml
+//
+//  Created by Nicolas Fabian Gramlich on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCNodeExporter : NSObject
+
+@end
---
+++ PlugIn Exporters/CCBAndEngineXml/Source/CCNodeExporter.m
@@ -0,0 +1,13 @@
+//
+//  CCNodeExporter.m
+//  CCBAndEngineXml
+//
+//  Created by Nicolas Fabian Gramlich on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CCNodeExporter.h"
+
+@implementation CCNodeExporter
+
+@end
